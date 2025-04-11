30e755baf4f6c9de2ebd73bbc90c4e101754d0a0
Cong Le <cle155@csu.fullerton.edu>
2025-04-11 13:43:38 -0700

    1st ty - nicely work

---
+++ PinPoint_SwiftUI_Source_code/PinPoint_SwiftUI/PinPoint_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinPoint_SwiftUIApp.swift
+//  PinPoint_SwiftUI
+//
+//  Created by Cong Le on 4/11/25.
+//
+
+import SwiftUI
+
+@main
+struct PinPoint_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
